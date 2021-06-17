b3abf460bc6befe5f5974c351a4968b88eb6efa0
Tanya <tanysa97@mail.ru>
2021-06-17 19:23:04 +0300

    add static root

--- storage_aggregator/settings.py
+++ storage_aggregator/settings.py
@@ -196,6 +196,7 @@ JWT_AUTH = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA
 MEDIA_URL = '/media/'
